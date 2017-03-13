f98bcb25ca082302dbc54d6dfaff5c8a2cb153a1
Tomáš Celizna <tomas.celizna@gmail.com>
2017-03-13 13:32:15 -0400

    strip whitespace at the beginning & end of string

--- lib/mongoid_permalink_extension/permalink.rb
+++ lib/mongoid_permalink_extension/permalink.rb
@@ -4,28 +4,25 @@ require 'stringex'
 
 module MongoidPermalinkExtension
   class Permalink
-
     class << self
-
-      def demongoize value
+      def demongoize(value)
         value
       end
 
-      def mongoize value
+      def mongoize(value)
         return if value.nil? # FIXME: this way we can never set the value to nil
-        value.to_s.
-          gsub(/([^[:upper:]](?=[[:upper:]]))|([a-z](?=\d+))/, '\1\2-').
-          gsub(/\//, '-').
-          gsub(/[-–—]+/, '-').
-          gsub(/[^\p{Alnum} -]/, '').
-          gsub(/\s+/, '-').
-          gsub(/[-]+/, '-').
-          gsub(/\A\-/, '').
-          gsub(/\b\w/){ $&.upcase }.
-          to_ascii
+        value.to_s
+             .gsub(/\A\s+|\s+\z/, '')
+             .gsub(/([^[:upper:]](?=[[:upper:]]))|([a-z](?=\d+))/, '\1\2-')
+             .gsub(/\//, '-')
+             .gsub(/[-–—]+/, '-')
+             .gsub(/[^\p{Alnum} -]/, '')
+             .gsub(/\s+/, '-')
+             .gsub(/[-]+/, '-')
+             .gsub(/\A\-/, '')
+             .gsub(/\b\w/) { $&.upcase }
+             .to_ascii
       end
-
     end
-
   end
 end
--- test/mongoid_permalink_extension/permalink_test.rb
+++ test/mongoid_permalink_extension/permalink_test.rb
@@ -27,52 +27,69 @@ describe MongoidPermalinkExtension::Permalink do
     it 'converts anything to string' do
       MongoidPermalinkExtension::Permalink.mongoize(12).must_equal '12'
     end
+
     it 'stores transformed string' do
-      MongoidPermalinkExtension::Permalink.mongoize("some some").must_be :present?
+      MongoidPermalinkExtension::Permalink.mongoize('some some').must_be :present?
     end
+
     it 'stores nil if nil passed' do
       MongoidPermalinkExtension::Permalink.mongoize(nil).must_be_nil
     end
+
     it 'converts spaces to hyphens' do
-      MongoidPermalinkExtension::Permalink.mongoize("A B").must_equal 'A-B'
-      MongoidPermalinkExtension::Permalink.mongoize("A    B").must_equal 'A-B'
+      MongoidPermalinkExtension::Permalink.mongoize('A B').must_equal 'A-B'
+      MongoidPermalinkExtension::Permalink.mongoize('A    B').must_equal 'A-B'
     end
+
+    it 'strips spaces around the string' do
+      MongoidPermalinkExtension::Permalink.mongoize('  A B  ').must_equal 'A-B'
+    end
+
     it 'converts slashes to hyphens' do
-      MongoidPermalinkExtension::Permalink.mongoize("A/B").must_equal 'A-B'
+      MongoidPermalinkExtension::Permalink.mongoize('A/B').must_equal 'A-B'
     end
+
     it 'converts to camel case' do
-      MongoidPermalinkExtension::Permalink.mongoize("hello hello").wont_include 'h'
-      MongoidPermalinkExtension::Permalink.mongoize("hello hello").must_equal 'Hello-Hello'
+      MongoidPermalinkExtension::Permalink.mongoize('hello hello').wont_include 'h'
+      MongoidPermalinkExtension::Permalink.mongoize('hello hello').must_equal 'Hello-Hello'
     end
+
     it 'adds spaces to camel case' do
-      MongoidPermalinkExtension::Permalink.mongoize("CamelCase").must_equal 'Camel-Case'
+      MongoidPermalinkExtension::Permalink.mongoize('CamelCase').must_equal 'Camel-Case'
     end
+
     it 'preserves ABC' do
-      MongoidPermalinkExtension::Permalink.mongoize("ABC Demo").must_equal 'ABC-Demo'
+      MongoidPermalinkExtension::Permalink.mongoize('ABC Demo').must_equal 'ABC-Demo'
       # MongoidPermalinkExtension::Permalink.mongoize("ABCDeMo").must_equal 'ABC-De-Mo'
     end
+
     it 'preserves dashes as separators' do
-      MongoidPermalinkExtension::Permalink.mongoize("Camel-Case").must_equal 'Camel-Case'
-      MongoidPermalinkExtension::Permalink.mongoize("Camel--Case").must_equal 'Camel-Case'
+      MongoidPermalinkExtension::Permalink.mongoize('Camel-Case').must_equal 'Camel-Case'
+      MongoidPermalinkExtension::Permalink.mongoize('Camel--Case').must_equal 'Camel-Case'
     end
+
     it 'converts all dashes to hyphens' do
-      MongoidPermalinkExtension::Permalink.mongoize("A–Z").must_equal 'A-Z'
-      MongoidPermalinkExtension::Permalink.mongoize("A----–Z").must_equal 'A-Z'
-      MongoidPermalinkExtension::Permalink.mongoize("A—Z").must_equal 'A-Z'
-      MongoidPermalinkExtension::Permalink.mongoize("A—––––––Z").must_equal 'A-Z'
+      MongoidPermalinkExtension::Permalink.mongoize('A–Z').must_equal 'A-Z'
+      MongoidPermalinkExtension::Permalink.mongoize('A----–Z').must_equal 'A-Z'
+      MongoidPermalinkExtension::Permalink.mongoize('A—Z').must_equal 'A-Z'
+      MongoidPermalinkExtension::Permalink.mongoize('A—––––––Z').must_equal 'A-Z'
     end
+
     it 'remove non-alphanumeric characters' do
       %w(?!@#$%^&*()+=.,;<>[]“”).shuffle.each do |i|
         MongoidPermalinkExtension::Permalink.mongoize(i).wont_include i
       end
     end
+
     it 'does not start with a hyphen' do
-      MongoidPermalinkExtension::Permalink.mongoize("“Yes” to a Historic Moment").must_equal "Yes-To-A-Historic-Moment"
-      MongoidPermalinkExtension::Permalink.mongoize("¿Yes?").must_equal "Yes"
+      MongoidPermalinkExtension::Permalink.mongoize('“Yes” to a Historic Moment').must_equal 'Yes-To-A-Historic-Moment'
+      MongoidPermalinkExtension::Permalink.mongoize('¿Yes?').must_equal 'Yes'
     end
+
     it 'preserves accented characters' do
       MongoidPermalinkExtension::Permalink.mongoize('Bienále Brno Česká republika').must_equal 'Bienale-Brno-Ceska-Republika'
     end
+
     it 'produces nice slugs' do
       MongoidPermalinkExtension::Permalink.mongoize('Future Vocabularies?').must_equal 'Future-Vocabularies'
       MongoidPermalinkExtension::Permalink.mongoize('2011 space odyssey').must_equal '2011-Space-Odyssey'
